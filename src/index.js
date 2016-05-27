1ff7e7ab518db637f6bd8d54e91a421518904003
Chris Sheppard <chris.sheppard@thebioagency.com>
2016-05-27 11:33:43 +0100

    Updates

--- src/index.js
+++ src/index.js
@@ -8,20 +8,59 @@ const path = require("path");
 const fm = require("front-matter");
 const MarkdownIt = require("markdown-it");
 const swig = require("swig");
-const koaBunyanLogger = require("koa-bunyan-logger");
+const koaLogger = require("koa-logger");
+const koaRateLimit = require("koa-better-ratelimit");
+const koaCompress = require("koa-compress");
 
 
 const CONTENT_PATH = path.normalize(`${__dirname}/../content/page`);
 const TEMPLATE_PATH = path.normalize(`${__dirname}/template/page`);
+const SWIG_OPTIONS = {
+    cache: false    // TODO: Only in development
+};
 
 const app = koa();
-app.use(koaBunyanLogger({ name: "Iki CMS", level: "warn" }));
-app.use(koaBunyanLogger.requestLogger());
 
+// Logging
+app.use(koaLogger());
+
+// Add rate limiting
+app.use(koaRateLimit({
+    duration: 1000, // 1 sec
+    max: 10,
+    blacklist: []
+}));
+
+// Add gzip compression
+app.use(koaCompress({
+    filter: function(content_type) {
+        // filter requests to be compressed using regex
+        return /text/i.test(content_type)
+    },
+    threshold: 860, //minimum size to compress
+    flush: require('zlib').Z_SYNC_FLUSH
+}));
+
+// Add markdown to the context
 app.context.md = new MarkdownIt({
     linkify: true,
     typographer: true
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 const getContentFilenameFromUrl = function(url) {
     let filename = path.join(CONTENT_PATH, path.normalize(url));
@@ -37,75 +76,65 @@ const getContentFromUrl = function(url) {
     return fs.readFile(getContentFilenameFromUrl(url), "utf8").then(fm);
 };
 
-app.use(function *(next) {
-    this.log.info('Got a request from %s for %s', this.request.ip, this.path);
-    yield next;
-});
-
-// Error handler
-app.use(function *(next) {
-    try {
-        yield next;
-    } catch (err) {
-        // TODO: Display server error page
-        this.status = err.status || 500;
-        this.body = err.message;
-        this.app.emit('error', err, this);
-    }
-});
-
-
-app.use(function *() {
+const page = function *(next) {
     let content, templateName;
 
     try {
         content = yield getContentFromUrl(this.request.url);
         templateName = content.attributes.template || 'default';
     } catch (err) {
-        this.app.emit("error", err, this);
-        this.status = 404;
-        content = {};
-        templateName = "404";
+        return yield next;
     }
 
-    let template = swig.compileFile(path.join(TEMPLATE_PATH, templateName) + '.swig', {
-        cache: false    // TODO: Only in development
-    });
+    let template = swig.compileFile(path.join(TEMPLATE_PATH, templateName) + '.swig', SWIG_OPTIONS);
 
     this.body = template({
         body: content.body ? this.md.render(content.body) : '',
         attributes: content.attributes
     });
-
-        // }).catch(err => {
-        //     this.status = err.status || 404;
-        //     this.body = '404' + err.status + err.message;
-        //     this.app.emit("error", err, this);
-        // });
+};
 
 
-    // yield getContentFromUrl(this.request.url)
-    //     .then(content => {
-    //
-    //         try {
-    //             let template = swig.compileFile(path.join(TEMPLATE_PATH, (content.attributes.template || 'default')) + '.swig', {
-    //                 cache: false    // TODO: Only in development
-    //             });
-    //
-    //             this.body = template({
-    //                 body: this.md.render(content.body),
-    //                 attributes: content.attributes
-    //             });
-    //         } catch (err) {
-    //             this.status = err.status || 500;
-    //             this.body = '500' + err.status + err.message;
-    //             this.app.emit("error", err, this);
-    //         }
-    //     }).catch(err => {
-    //         this.status = err.status || 404;
-    //         this.body = '404' + err.status + err.message;
-    //         this.app.emit("error", err, this);
-    //     });
+
+
+
+// app.use(function *(next) {
+//     this.log.info('Got a request from %s for %s', this.request.ip, this.path);
+//     yield next;
+// });
+
+app.use(page);
+
+// Error handler
+app.use(function *serverError(next) {
+    try {
+        yield next;
+    } catch (err) {
+        // TODO: Display server error page
+        this.status = err.status || 500;
+
+        let template = swig.compileFile(path.join(TEMPLATE_PATH, ""+this.status) + '.swig', SWIG_OPTIONS);
+
+        this.body = template({
+            message: err.message
+        });
+        this.app.emit('error', err, this);
+    }
+});
+
+app.use(function *pageNotFound(next) {
+    yield next;
+
+    if (this.status !== 404) {
+        return;
+    }
+
+    // Need to explicitly set 404 so that koa doesn't assign 200 on body
+    this.status = 404;
+
+    let template = swig.compileFile(path.join(TEMPLATE_PATH, ""+this.status) + '.swig', SWIG_OPTIONS);
+
+    this.body = template();
 });
 
 
@@ -115,7 +144,7 @@ app.use(function *() {
 // https://github.com/Carlangueitor/winston-koa-logger
 app.on('error', function(err) {
     // log.error('server error', err);
-    // console.error('server error', err);
+    console.error('server error', err);
 });
 
 app.listen(8080);
